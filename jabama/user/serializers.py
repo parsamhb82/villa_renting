64e5f86a1d7f4b48cef8ed703b859826dfa98ce7
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-31 21:34:26 +0330

    user and userregistration

---
+++ jabama/user/serializers.py
@@ -0,0 +1,27 @@
+from .models import UserProfile
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'username', 'email', 'bio', 'money_wallet']
+
+class UserProfileRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirmation = serializers.CharField(write_only=True)
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'username', 'email', 'password', 'password_confirmation']
+    
+    def validate(self, data):
+        if data['password']!= data['password_confirmation']:
+            raise serializers.ValidationError('Passwords do not match.')
+        return data
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        return user
+
--- jabama/user/views.py
+++ jabama/user/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from user.serializers import UserProfileSerializer, UserProfileRegisterSerializer
+from user.models import UserProfile
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+class Login(TokenObtainPairView):
+    pass
+
+class Refresh(TokenRefreshView):
+    pass
+
+class UserProfileList(ListAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class UserProfileView(RetrieveAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+class UserRegistrationView(CreateAPIView):
+    serializer_class = UserProfileRegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        user_serializer = self.get_serializer(data=request.data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+
+        profile_data = {
+            'bio' : request.data.get('bio', ''),
+            'money_wallet' : request.data.get('money_wallet', 0),
+        }
+
+        UserProfile.objects.create(user=user, **profile_data)
+
+        return JsonResponse({'message' : 'user registered successfuly'}, status = 201)
+
+
