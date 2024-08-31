da070dee0157ea0931086873012e80b3e5e50420
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-31 17:48:02 +0330

    models created

--- jabama/user/models.py
+++ jabama/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    money_wallet = models.IntegerField(default=0)
+    
+
+
+
+    
+
+    
--- jabama/villa/models.py
+++ jabama/villa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from user.models import UserProfile
 
-# Create your models here.
+
+
+class Villa(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name = 'owner')
+    past_renters = models.ManyToManyField(UserProfile, related_name='renters', blank=True)
+    current_renter = models.ForeignKey(UserProfile, on_delete= models.PROTECT, related_name='current_renter', blank=True, null=True)
+    is_currently_rented = models.BooleanField(default=False)
+    city = models.CharField(max_length=255)
+    address = models.TextField()
+    features = models.TextField()
+
+
+
+
+
