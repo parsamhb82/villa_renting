406604cd14f1bcb73f406fbb500f07470979bd9d
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-02 18:28:10 +0330

    registration handled

--- jabama/user/serializers.py
+++ jabama/user/serializers.py
@@ -14,8 +14,8 @@ class UserProfileRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     password_confirmation = serializers.CharField(write_only=True)
     class Meta:
-        model = Customer
-        fields = ['id', 'username', 'email', 'password', 'password_confirmation']
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'password_confirmation', 'first_name', 'last_name']
     
     def validate(self, data):
         if data['password']!= data['password_confirmation']:
@@ -23,7 +23,7 @@ class UserProfileRegisterSerializer(serializers.ModelSerializer):
         return data
     
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(username=validated_data['username'], email=validated_data['email'], password=validated_data['password'], first_name=validated_data['first_name'], last_name=validated_data['last_name'])
         return user
 
 class OwnerSerializer(serializers.ModelSerializer):
--- jabama/user/urls.py
+++ jabama/user/urls.py
@@ -1,6 +1,10 @@
 
 from django.urls import path
-from . import views
+from .views import (Login,
+                    Refresh,
+                    UserProfileList,
+                    UserProfileView,
+                    )
 
 urlpatterns = [
         path("create_owner/", views.CreateOwnerView.as_view())
